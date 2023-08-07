f6a8618dda846978293a8539ec386cadbcc4e338
Axolotlbyte <arjaankhan@gmail.com>
2023-08-08 01:54:19 +0530

    bismillah added

--- components/navigation/Navigation.js
+++ components/navigation/Navigation.js
@@ -2,15 +2,15 @@ import { navlinks } from "../../constants";
 import NavList from "./NavList";
 
 const Navigation = () => {
-    return (
-        <nav className="fixed top-6 right-6 h-auto z-50 text-teal-500">
-        <ul className="grid gap-4 text-2xl lg:text-4xl">
-          {navlinks.map((link) => (
-            <NavList key={link.label} label={link.label} />
-          ))}
-        </ul>
-      </nav>
-    )
-}
+  return (
+    <nav className="fixed top-20 right-6 h-auto z-50 text-teal-500">
+      <ul className="grid gap-4 text-2xl lg:text-4xl">
+        {navlinks.map((link) => (
+          <NavList key={link.label} label={link.label} />
+        ))}
+      </ul>
+    </nav>
+  );
+};
 
 export default Navigation;
